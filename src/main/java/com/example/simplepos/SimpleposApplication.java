b58d5f2f6a6efb908b0c75e99eedc8d0708e202f
samruddhi1499 <samchavanxiv9@gmail.com>
2024-06-10 11:29:18 -0400

    starting with project

---
+++ src/main/java/com/example/simplepos/SimpleposApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleposApplication.class, args);
+	}
+
+}
